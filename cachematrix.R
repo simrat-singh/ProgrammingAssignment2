b1eb0fe6ea0a2ec2a81e8276acba2180d083a223
simrat-singh <simratsingh.ds@gmail.com>
2016-10-06 21:01:44 +1100

    Addind code changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+#These functions checks for cached inversed matrix for the input matrix.
+#If the inversed matrix exists it displays the value else it inverse the
+#matrix and cache it.
 
-## Write a short comment describing this function
 
+#This function creates a list of objects to be consumed and also retain cache
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  set <- function(inv_data) inv <<- inv_data
+  getinv <- function() inv
+  list(set = set, get = get, setinv = set, getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
+#This function inverses the matrix and also set cache if no data is cached
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m<-solve(data)
+  x$setinv(m)
+  m
 }
+
+#Test cases
+#library(datasets)
+#iris_subset<-iris[1:4,1:4]
+#m<-makeCacheMatrix(iris_subset)
+#cacheSolve(m)
